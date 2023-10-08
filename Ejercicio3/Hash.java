98871c486356df44ed0b0614d3444f4b65763e37
Carlos Manuel Alvarez Lopez <carlosmal1502@gmail.com>
2023-10-07 21:33:53 -0600

    Hash++

--- Ejercicio3/Hash.java
+++ Ejercicio3/Hash.java
@@ -6,4 +6,11 @@ public class Hash{
         return (n%15);
     }
 
+    public static int cuadrado(int n){
+        String s = (Integer.toString(n*n));
+        int mid = s.length()/2;
+        s = s.substring(mid-1, mid+1);
+        return Integer.valueOf(s)%15;
+    }
+
 }
